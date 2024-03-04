d59360ed401605f3c5bb5894d3fdee32ea69da0a
nathsoto1 <nsoto1@lsu.edu>
2024-03-04 11:35:53 -0600

    First Commit
    03-04-2024

---
+++ encrypt_password/encrypt_passwords.py
@@ -0,0 +1,37 @@
+from cryptography.fernet import Fernet
+
+master_pwd = input("What isd the master password? ")
+
+def write_key():
+  key = Fernet.generate_key()
+  with open("key.key", "wb") as key_file:
+    key_file.write(key)
+
+write_key()
+
+def view():
+  with open('password.txt', 'r') as f:
+    for line in f.readlines():
+      data = line.rstrip()
+      user, passw = data.split("|")
+      print("User:", user, ", Password", passw)
+
+def add():
+  name = input("Account name: ")
+  pwd = input("Password: ")
+
+  with open('password.txt', 'a') as f:
+    f.write(name + '|' + pwd + "\n")
+
+while True:
+  mode = input("Would you like to add a new password or view visting ones (view, add), press q to quit? " ).lower()
+  if mode == "q":
+    break
+
+  if mode == "view":
+    view()
+  elif mode == "add":
+    add()
+  else:
+    print("Invalid mode.")
+    continue
